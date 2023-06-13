f9eb27c90604d66751a0052cfec1b5a26d38c7fd
marcelvini <marcelviniccius@gmail.com>
2023-06-13 00:49:22 -0300

    add swagger documentation

--- src/auth/dto/authDto.ts
+++ src/auth/dto/authDto.ts
@@ -1,5 +1,14 @@
-type authDto = {
+import { ApiProperty } from '@nestjs/swagger';
+class authDto {
+  @ApiProperty({
+    example: 'any@email.com',
+    description: 'its the user email.',
+  })
   email: string;
+  @ApiProperty({
+    example: '123456',
+    description: 'its the user password.',
+  })
   password: string;
-};
+}
 export { authDto };
